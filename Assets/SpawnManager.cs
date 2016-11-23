a808fb79330c1204cc518cf6fec8520a8fe58442
VincentMorin <vincent.morin1@gmail.com>
2016-11-23 16:28:15 -0500

    spawnManager pour le monstres

---
+++ Assets/SpawnManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnManager : MonoBehaviour {
+    public int timeBetweenSpawn;
+    private float timeToNextSpawn;
+	// Use this for initialization
+	void Start () {
+        timeBetweenSpawn = 5;
+        timeToNextSpawn = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timeToNextSpawn += Time.deltaTime;
+        if (timeToNextSpawn >= timeBetweenSpawn )
+        {
+     /*     GameObject go = (GameObject)Instantiate(Resources.Load("MyPrefab"));  // need to test with a prefab
+            go.transform.position = this.transform.position;
+            timeToNextSpawn = 0; 
+            */
+        }
+	}
+}
