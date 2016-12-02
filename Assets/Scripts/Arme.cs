4f5f46f859c5ddb3911d9c1e6566e3e13dc91f2b
VincentMorin <vincent.morin1@gmail.com>
2016-12-02 09:09:05 -0500

    ajout du son pour le menu, ajout de la classe Arme et d'un Clavier

---
+++ Assets/Scripts/Arme.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arme : MonoBehaviour {
+
+    protected string nom;
+    protected int durabilite;
+    protected float degatBase;
+    protected float vitesseAtt;
+
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            //Launch The animation here
+            //chech for a hit 
+            GameObject[] monsters;
+            monsters = GameObject.FindGameObjectsWithTag("Monster");
+
+            foreach (GameObject monster in monsters)
+            {
+                //call the hit here
+                monster.GetComponent<>();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Clavier.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clavier: Arme {
+
+    public Clavier()
+    {
+        this.nom = "clavier";
+        this.degatBase = 3;
+        this.vitesseAtt = 1;
+        this.durabilite = 2;
+    }
+}
