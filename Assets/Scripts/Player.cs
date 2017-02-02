a54a2ba226e2785d1ed555da83e1c963f8110277
Mathieu Victor Guérin <guerinmathieu19@gmail.com>
2017-02-02 17:42:20 -0500

    Ajout de deux méthode de déplacement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,28 +4,58 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
 
+    const int MaxHitPoint = 100;
+    public int hitPoint = MaxHitPoint;
     public Item[] inventory = new Item[8];
     Weapon activeWeapon = null;
     Weapon backupWeapon = null;
     public float speed;
     private Rigidbody2D rb;
+    private Vector3 target; 
+
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        target = transform.position;
     }
 
     // Update is called once per frame
-    void FixedUpdate () {
+    //void FixedUpdate () {
 
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+    /*First script movement with translation, need to configure the speed parametre and make sure you have a RB2D On
 
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-        rb.AddForce(movement);
-    //    this.transform.position.x += moveVertical * speed;
-     //   this.transform.position.y += moveVertical * speed;
+    float moveHorizontal = Input.GetAxis("Horizontal");
+    float moveVertical = Input.GetAxis("Vertical");
+
+    Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+    rb.AddForce(movement);
+    this.transform.position.x += moveVertical * speed;
+    this.transform.position.y += moveVertical * speed;
+    */
+
+    /*Second Script movement with forces ,  need to configure the speed parametre and make sure you have a RB2D On
+
+    float moveHorizontal = Input.GetAxis("Horizontal");
+    float moveVertical = Input.GetAxis("Vertical");
+    Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+    rb.AddForce(movement*speed);
+    */
+
+
+    //}
+    /* Third script movement with OnClick command, need to configure the speed parametre. You dont NEED RB2D.*/
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            target.z = transform.position.z;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
     }
+
     public void AddItem(Item newItem)
     {
         int i = 0;
