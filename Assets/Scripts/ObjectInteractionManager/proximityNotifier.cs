767699b946b8e4aca4c3d4db827c2fa58ef47ece
MathieuAuclair <mathiass12@hotmail.com>
2017-07-22 18:15:29 -0400

    adding a Proximity notifier

---
+++ Assets/Scripts/ObjectInteractionManager/proximityNotifier.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class proximityNotifier : MonoBehaviour {
+	Behaviour HaloSwitch;
+
+	void Start(){
+		HaloSwitch = (gameObject.GetComponent ("Halo") as Behaviour);
+	}
+
+	void OnTriggerEnter2D(Collider2D player){
+		if (player.gameObject.tag == "Player") {
+			HaloSwitch.enabled = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D player){
+		if (player.gameObject.tag == "Player") {
+			HaloSwitch.enabled = false;
+		}
+	}
+}
